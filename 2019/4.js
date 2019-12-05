7a6eea1e8abfda6d010bc78171dcaea929b633e6
David Mine <dhmine@gmail.com>
2019-12-04 22:55:51 -0500

    Faster way to do AoC 2019.4
    Instead of looping through every int in the range, I now do something
    different.
    
    I find the first number higher than the low range that does not
    decrement between the digits.
    
    I pass this initial number into a validNumberGenerator. This removes all
    "9"s from the end of the number, increments the (now smaller) number,
    and appends the last digit until the length of the string is 6. If the
    new number is within range, push to the output array.
    
    This greatly reduces the initial set of valid numbers. I then loop
    through this smaller set to check for duplicates. The code is much
    faster now.

--- 2019/4.js
+++ 2019/4.js
@@ -18,37 +18,71 @@ function startup(puzzleInput){
 
 function CalculateRange(stringInput) {
   var range = {low: 0, high: 0};
-  range.low = stringInput.split("-")[0];
-  range.high = stringInput.split("-")[1];
+  range.low = parseInt(stringInput.split("-")[0]);
+  range.high = parseInt(stringInput.split("-")[1]);
   return range;
 }
 
 function GeneratePotentialPasswords(range) {
-  var potentialPasswords = [];
-  for (var i = range.low; i <= range.high; i++){
-    if (IsPotentialPassword(i)) {
-      potentialPasswords.push(i);
-    }
-  }
+  var potentialPasswords = NumbersInRangeWithoutDecrementingDigits(range.low, range.high); // generate the passwords that have non-decrementing digits
+  console.log(potentialPasswords);
+  potentialPasswords = potentialPasswords.filter(password => HasTwoSameDigitsInARow(password)); // filter out the ones that don't have two same digits in a row
   return potentialPasswords;
 }
 
-function IsPotentialPassword(potentialPassword) {
-  console.log(potentialPassword);
-  if (!IsIncreasingDigitValue(potentialPassword) || !HasTwoSameDigitsInARow(potentialPassword)){
-    return false;
+function NumbersInRangeWithoutDecrementingDigits(lowNumber, highNumber) {
+  var validNumber = GenerateInitialNonDerementingNumber(lowNumber);
+  var generatedPasswords = [];
+  while (validNumber <= highNumber){
+
+    var baseNumberAsString = (Remove9sFromEndOfNumber(validNumber) + 1).toString();
+    
+    if (baseNumberAsString.length < 6) {
+      var lastDigit = parseInt(baseNumberAsString[baseNumberAsString.length - 1]);
+      while (baseNumberAsString.length < 6) {
+        baseNumberAsString += lastDigit;
+      }
+    }
+
+    validNumber = parseInt(baseNumberAsString);
+    generatedPasswords.push(validNumber);
   }
 
-  return true;
+  return generatedPasswords;
 }
 
-function IsIncreasingDigitValue(potentialPassword) {
-  var potentialPasswordAsArray = potentialPassword.toString();
-  return parseInt(potentialPasswordAsArray[0]) <= parseInt(potentialPasswordAsArray[1]) &&
-         parseInt(potentialPasswordAsArray[1]) <= parseInt(potentialPasswordAsArray[2]) &&
-         parseInt(potentialPasswordAsArray[2]) <= parseInt(potentialPasswordAsArray[3]) &&
-         parseInt(potentialPasswordAsArray[3]) <= parseInt(potentialPasswordAsArray[4]) &&
-         parseInt(potentialPasswordAsArray[4]) <= parseInt(potentialPasswordAsArray[5]);
+function GenerateInitialNonDerementingNumber(num) {
+
+  var numAsString = num.toString();
+  var indexOfFirstDecrementDigit = 0;
+
+  // find the index where things go wrong. If none, nothing will happen
+  for (var i = 1; i < numAsString.length; i++) {
+    if (parseInt(numAsString[i-1]) > parseInt(numAsString[i]) && indexOfFirstDecrementDigit == 0) {
+      indexOfFirstDecrementDigit = i;
+    }
+  }
+
+  // if there's a decrement between digits, overwrite the rest of the string with the last digit before the decrement
+  if (indexOfFirstDecrementDigit > 0) {
+    numAsString = numAsString.substr(0, indexOfFirstDecrementDigit);
+    var lastDigit = numAsString[numAsString.length - 1];
+
+    while (numAsString.length < 6) {
+      numAsString += lastDigit;
+    }
+  }
+  return parseInt(numAsString);
+}
+
+// this function will remove all the 9s from the end of the number
+// for example, the number 199999 will return 1. 349 will return 34
+function Remove9sFromEndOfNumber(num) {
+  if (num % 10 == 9) {
+    num = Remove9sFromEndOfNumber(parseInt(num / 10));
+  }
+
+  return num;
 }
 
 function HasTwoSameDigitsInARow(potentialPassword) {
@@ -56,5 +90,5 @@ function HasTwoSameDigitsInARow(potentialPassword) {
   var uniqueDigitsInPotentialPassword = Array.from(new Set(potentialPasswordAsArray));
 
   return uniqueDigitsInPotentialPassword.some(digit => potentialPasswordAsArray.lastIndexOf(digit) - potentialPasswordAsArray.indexOf(digit) == 1);
-
 }
+
